4522912b23ce96428d192a27bbef312176c786c9
yyhk123 <minoook@hotmail.com>
2020-10-08 01:09:01 -0500

    mern added

--- src/components/projects.js
+++ src/components/projects.js
@@ -264,6 +264,40 @@ class Projects extends Component {
                     width: "auto",
                     color: "black",
                     background:
+                      "url(https://i.imgur.com/ISOUDEq.png) bottom right 15% no-repeat #46B6AC",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}
+                ></CardTitle>
+                <CardText>MERN Login</CardText>
+                <CardActions border>
+                  <Button
+                    colored
+                    href="https://github.com/yyhk123/MERN"
+                    target="_blank"
+                  >
+                    Github
+                  </Button>
+                </CardActions>
+              </Card>
+            </Cell>
+
+            <Cell col={4} className="card">
+              <Card
+                shadow={0}
+                style={{
+                  width: "auto",
+                  height: "100%",
+                  margin: "auto",
+                }}
+              >
+                <CardTitle
+                  expand
+                  style={{
+                    width: "auto",
+                    color: "black",
+                    background:
                       "url(https://i.imgur.com/jxk9JTL.png) bottom right 15% no-repeat #46B6AC",
                     backgroundPosition: "center",
                     backgroundRepeat: "no-repeat",
