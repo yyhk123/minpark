c73e3eb66d58702b74c3e36cf037d6a9c2d1abcd
yyhk123 <minoook@hotmail.com>
2020-12-26 02:48:15 -0600

    added idolcup

--- src/components/projects.js
+++ src/components/projects.js
@@ -388,6 +388,43 @@ class Projects extends Component {
               </Card>
             </Cell>
 
+            <Cell col={4} className="card">
+              <Card
+                shadow={0}
+                style={{
+                  width: "auto",
+                  height: "100%",
+                  margin: "auto",
+                }}
+              >
+                <CardTitle
+                  expand
+                  style={{
+                    width: "auto",
+                    color: "black",
+                    background:
+                      "url(https://i.imgur.com/I2kFzFq.png) bottom right 15% no-repeat #46B6AC",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}
+                ></CardTitle>
+                <CardText>Android App - IdolCup</CardText>
+                <CardActions border>
+                  <Button
+                    colored
+                    href="https://github.com/yyhk123/IdolCup/tree/master"
+                    target="_blank"
+                  >
+                    Github
+                  </Button>
+                  <Button colored href="https://play.google.com/store/apps/details?id=com.min.idolcup" target="_blank">
+                    Play Store
+                  </Button>
+                </CardActions>
+              </Card>
+            </Cell>
+
           </Grid>
         </Fade>
       </div>
