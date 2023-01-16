2e26d56e650cc415ea70e0b044d11e41efd152ce
yyhk123 <minoook@hotmail.com>
2023-01-16 03:23:02 -0600

    Revert "Update experiences.js"
    This reverts commit b9e09bbcd0ded0b6d8c554954db3f76ad942ef39.

--- src/components/experiences.js
+++ src/components/experiences.js
@@ -18,7 +18,8 @@ class Experiences extends Component {
                   style={{ width: "calc(66.6666666667% - 16px)" }}
                 >
                   <h2 className="experiences-title">
-                    MGM Resorts International <br />
+                    SAMSUNG ELECTRONICS AMERICA <br />
+                    (Contingent work)
                   </h2>
                 </Cell>
                 <Cell
@@ -26,25 +27,22 @@ class Experiences extends Component {
                   className="mdl-cell--4-col"
                   style={{ width: "calc(33.3333333333% - 16px)" }}
                 >
-                  <h2 className="experiences-date">January 2022 - present</h2>
+                  <h2 className="experiences-date">March 2021 - present</h2>
                 </Cell>
               </Grid>
               <section className="expereinces-detail-container">
-                <h3 className="experiences-position">SOFTWARE ENGINEER</h3>
+                <h3 className="experiences-position">SOFTWARE DEVELOPER</h3>
                 <p className="experiences-detail">
-                  - Developed and maintain high availability for MGM Resorts International's customer 
-                  facing website that allows guests to book room reservations using Java..
+                  - Assisted in development and maintenance of new APIs for automated 
+                  testings on mobile devices including smartphone, tablets and wearable devices.
                 </p>
                 <p className="experiences-detail">
-                  - Contribute to creating solutions in GraphQL using TypeScript to ensure a smooth 
-                  orchestration layer experience between our frontend and backend services.
+                  - Function as a team member in design, development, troubleshoot, test and 
+                  analyze software projects involving mobile application.
                 </p>
                 <p className="experiences-detail">
-                  - Created Key Performance Indicators (KPI's) using Power Bi to allow business to 
-                  monitor cart and order services health and utilization.
-                </p>
-                <p className="experiences-detail">
-                  - Collaborated with cross-functional teams to troubleshoot and resolve technical issues.
+                  - Modify existing software to correct errors and reduce time complexity 
+                  for performance and efficiency.
                 </p>
               </section>
             </div>
@@ -66,26 +64,67 @@ class Experiences extends Component {
                   className="mdl-cell--4-col"
                   style={{ width: "calc(33.3333333333% - 16px)" }}
                 >
-                  <h2 className="experiences-date">May 2020 - December 2021</h2>
+                  <h2 className="experiences-date">May 2020 - March 2021</h2>
                 </Cell>
               </Grid>
               <section className="expereinces-detail-container">
                 <h3 className="experiences-position">SOFTWARE QA ENGINEER 1</h3>
                 <p className="experiences-detail">
-                  - Provide data application development and support, including QA 
-                  regression testing of hardware/software, troubleshooting Android OS, 
-                  Windows Mobile OS, and Qualcomm BREW environments.
+                  - Test and troubleshoot hardware, software, and embedded
+                  software used in wireless data networking for CDMA/LTE.
+                  phones.
                 </p>
                 <p className="experiences-detail">
-                  - Conduct test automation activities for android mobile devices including creating, 
-                  executing and maintaining test scripts using Android Control Tester(ACT).
+                  - Provide data application development and support, including
+                  QA regression testing of hardware/software, troubleshooting
+                  Android OS, Windows Mobile OS, and Qualcomm BREW environments.
                 </p>
                 <p className="experiences-detail">
-                  - Worked in a team of creating ML web scraping tool, using Scrapy Python. 
-                  Developed and maintained reports and test results.
+                  - Conduct test automation activities including creating,
+                  executing and maintaining test scripts using Android Control
+                  Tester(ACT).
                 </p>
               </section>
             </div>
+
+            <div style={{ padding: "25px 8px 8px 8px" }}>
+              <Grid style={{ margin: "0px" }}>
+                <Cell
+                  col={8}
+                  className="mdl-cell--8-col"
+                  style={{ width: "calc(66.6666666667% - 16px)" }}
+                >
+                  <h2 className="experiences-title">
+                    ALL STAR VERIZON WIRELESS
+                  </h2>
+                </Cell>
+                <Cell
+                  col={4}
+                  className="mdl-cell--4-col"
+                  style={{ width: "calc(33.3333333333% - 16px)" }}
+                >
+                  <h2 className="experiences-date">
+                    November 2013 - April 2020
+                  </h2>
+                </Cell>
+              </Grid>
+              <section className="expereinces-detail-container">
+                <h3 className="experiences-position">SALES REPRESENTATIVE</h3>
+                <p className="experiences-detail">
+                  - Perform troubleshooting of customer devices to diagnose and
+                  repair issues with devices.
+                </p>
+                <p className="experiences-detail">
+                  - Demonstrate knowledge of products, applying solution-selling
+                  techniques to meet needs of customers.
+                </p>
+                <p className="experiences-detail">
+                  - Use Verizon Wireless internal database system to add,
+                  remove, or edit information accurately.
+                </p>
+              </section>
+            </div>
+
             
           </Fade>
         </div>
