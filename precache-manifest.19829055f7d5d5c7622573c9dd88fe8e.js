0515584ada7b2e1b8776df4e7176805d01770ece
yyhk123 <minoook@hotmail.com>
2023-02-23 02:16:54 -0600

    Updates

---
+++ precache-manifest.19829055f7d5d5c7622573c9dd88fe8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1afd31be2e52f430a7387a5706ece7",
+    "url": "/minpark/index.html"
+  },
+  {
+    "revision": "73d97dc71dc7a4eb6890",
+    "url": "/minpark/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "61af79b0eff415e11695",
+    "url": "/minpark/static/css/main.638f5a56.chunk.css"
+  },
+  {
+    "revision": "73d97dc71dc7a4eb6890",
+    "url": "/minpark/static/js/2.abb878d5.chunk.js"
+  },
+  {
+    "revision": "f8cc9d9189f32725652d20779ca2808e",
+    "url": "/minpark/static/js/2.abb878d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61af79b0eff415e11695",
+    "url": "/minpark/static/js/main.7c77c55a.chunk.js"
+  },
+  {
+    "revision": "d5ccb22460e5cfa52003",
+    "url": "/minpark/static/js/runtime-main.9db4dcc9.js"
+  },
+  {
+    "revision": "1552140c301ae3145d047a607779f91d",
+    "url": "/minpark/static/media/main.1552140c.jpg"
+  }
+]);
